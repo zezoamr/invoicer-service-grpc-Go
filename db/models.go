bc593d43930a7010eadcb13b2eab0b1e129c9261
ziad amr shalaby <zezoamr@yandex.com>
2023-07-15 02:17:02 +0300

    Update models.go

--- db/models.go
+++ db/models.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 type Invoice struct {
 	gorm.Model
-	FromID        uint `gorm:"not null"`
-	ToID          uint `gorm:"not null"`
+	FromID        uint32 `gorm:"not null"`
+	ToID          uint32 `gorm:"not null"`
 	Voice         []byte
 	Words         []string
 	WordsNotAudio bool `gorm:"not null"`
