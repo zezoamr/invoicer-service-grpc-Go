b649f6f09aed126c2b7d8eb5b34375e911fa8ded
ziad amr shalaby <zezoamr@yandex.com>
2023-07-15 02:17:16 +0300

    Update invoicerServer.go

--- invoicerServer.go
+++ invoicerServer.go
@@ -10,6 +10,14 @@ type invoicerServer struct {
 	invoicer.UnimplementedInvoicerServer
 }
 
-func (*invoicerServer) Create(context.Context, *invoicer.CreateRequest) (*invoicer.CreateResponseNoParmas, error) {
-	return &invoicer.CreateResponseNoParmas{}, nil
+func (*invoicerServer) SendVoiceMail(ctx context.Context, req *invoicer.CreateRequest) (*invoicer.SendRequestStatus, error) {
+	status, id := dbmSendVoiceMail(db, Invoice{
+		Voice:         req.Message.Audio,
+		Words:         req.Message.Words,
+		WordsNotAudio: req.Message.Wordsnotaudio,
+		FromID:        req.Userfrom,
+		ToID:          req.Userto,
+		Seen:          false,
+	})
+	return &invoicer.SendRequestStatus{Status: status, Messageid: uint32(id)}, nil
 }
