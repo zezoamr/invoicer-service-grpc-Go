1bd12d2f32e81585ca5513495eecf61f3f14fa45
ziad amr shalaby <zezoamr@yandex.com>
2023-07-16 01:41:47 +0300

    Update invoicerServer.go

--- invoicerServer.go
+++ invoicerServer.go
@@ -22,3 +22,22 @@ func (*invoicerServer) SendVoiceMail(ctx context.Context, req *invoicer.CreateRe
 	})
 	return &invoicer.SendRequestStatus{Status: status, Messageid: uint32(id)}, nil
 }
+
+func (*invoicerServer) ReadUnSeenReceived(ctx context.Context, req *invoicer.ReadRequest) (*invoicer.MultipleinvoiceReply, error) {
+	messages := db.DbReadUnSeenReceived(dbconn, uint(req.Userid), int(req.Skip), int(req.Limit))
+
+	grpcItems := make([]*invoicer.InvoiceReply, len(messages))
+	for i, item := range messages {
+		grpcItems[i] = &invoicer.InvoiceReply{
+			ID:            uint32(item.ID),
+			Audio:         item.Voice,
+			Words:         item.Words,
+			Wordsnotaudio: item.WordsNotAudio,
+			FromID:        item.FromID,
+			ToID:          item.ToID,
+			Seen:          item.Seen,
+		}
+	}
+
+	return &invoicer.MultipleinvoiceReply{InvoiceReply: grpcItems}, nil
+}
