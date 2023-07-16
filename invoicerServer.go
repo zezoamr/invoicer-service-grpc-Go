67e86b1ce840f6bbe58d52c083e4244ccb71a061
ziad amr shalaby <zezoamr@yandex.com>
2023-07-16 04:40:40 +0300

    Update invoicerServer.go

--- invoicerServer.go
+++ invoicerServer.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"context"
+	"io"
+	"log"
 
 	db "github.com/zezoamr/invoicer-service-grpc-Go/db"
 	"github.com/zezoamr/invoicer-service-grpc-Go/proto/invoicer"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type invoicerServer struct {
 	invoicer.UnimplementedInvoicerServer
 }
 
-func (*invoicerServer) SendVoiceMail(ctx context.Context, req *invoicer.CreateRequest) (*invoicer.SendRequestStatus, error) {
+func (s *invoicerServer) SendVoiceMail(ctx context.Context, req *invoicer.CreateRequest) (*invoicer.SendRequestStatus, error) {
 	status, id := db.DbSendVoiceMail(dbconn, db.Invoice{
 		Voice:         req.Message.Audio,
 		Words:         req.Message.Words,
@@ -23,7 +26,7 @@ func (*invoicerServer) SendVoiceMail(ctx context.Context, req *invoicer.CreateRe
 	return &invoicer.SendRequestStatus{Status: status, Messageid: uint32(id)}, nil
 }
 
-func (*invoicerServer) ReadUnSeenReceived(ctx context.Context, req *invoicer.ReadRequest) (*invoicer.MultipleinvoiceReply, error) {
+func (s *invoicerServer) ReadUnSeenReceived(ctx context.Context, req *invoicer.ReadRequest) (*invoicer.MultipleinvoiceReply, error) {
 	messages := db.DbReadUnSeenReceived(dbconn, uint(req.Userid), int(req.Skip), int(req.Limit))
 
 	grpcItems := make([]*invoicer.InvoiceReply, len(messages))
@@ -40,4 +43,67 @@ func (*invoicerServer) ReadUnSeenReceived(ctx context.Context, req *invoicer.Rea
 	}
 
 	return &invoicer.MultipleinvoiceReply{InvoiceReply: grpcItems}, nil
+}
+
+func (s *invoicerServer) ReadAllReceived(req *invoicer.ReadRequest, stream invoicer.Invoicer_ReadAllReceivedServer) error {
+	log.Printf("Got request for invoices for user : %v", req.Userid)
+
+	items := db.DbReadAllReceived(dbconn, uint(req.Userid), int(req.Skip), int(req.Limit))
+	for _, item := range items {
+		res := &invoicer.InvoiceReply{
+			ID:            uint32(item.ID),
+			Audio:         item.Voice,
+			Words:         item.Words,
+			Wordsnotaudio: item.WordsNotAudio,
+			FromID:        item.FromID,
+			ToID:          item.ToID,
+			Seen:          item.Seen,
+		}
+
+		// 2 second delay to simulate a long running process
+		//time.Sleep(2 * time.Second)
+		if err := stream.Send(res); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (s *invoicerServer) ReadAllSent(req *invoicer.ReadRequest, stream invoicer.Invoicer_ReadAllSentServer) error {
+	log.Printf("Got request for invoices from user : %v", req.Userid)
+
+	items := db.DbReadAllSent(dbconn, uint(req.Userid), int(req.Skip), int(req.Limit))
+	for _, item := range items {
+		res := &invoicer.InvoiceReply{
+			ID:            uint32(item.ID),
+			Audio:         item.Voice,
+			Words:         item.Words,
+			Wordsnotaudio: item.WordsNotAudio,
+			FromID:        item.FromID,
+			ToID:          item.ToID,
+			Seen:          item.Seen,
+		}
+
+		// 2 second delay to simulate a long running process
+		//time.Sleep(2 * time.Second)
+		if err := stream.Send(res); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (s *invoicerServer) ReadMessageTime(stream invoicer.Invoicer_ReadMessageTimeServer) error {
+	var messages []*invoicer.ReadMessageTime
+	for {
+		req, err := stream.Recv()
+		status, seen, time := db.DbReadMessageTime(dbconn, uint(req.Messageid))
+		messages = append(messages, &invoicer.ReadMessageTime{Status: status, Read: seen, Readtime: timestamppb.New(time)})
+		if err == io.EOF {
+			return stream.SendAndClose(&invoicer.ReadMessageTimeResponse{SeenArray: messages})
+		}
+		if err != nil {
+			return err
+		}
+	}
 }
